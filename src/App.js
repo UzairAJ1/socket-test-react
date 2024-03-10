58afc38e23ab059dc11ed563f937184724a01113
Uzair Aj <53966756+UzairAJ1@users.noreply.github.com>
2024-03-11 03:44:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import io from "socket.io-client";
+import React, { useEffect, useState } from "react";
 function App() {
+  useEffect(() => {
+    // Connect to the WebSocket server
+    const socket = io("http://localhost:3333");
+    //Enter the userId 
+    const userId="1";
+socket.on("connect", () => {
+ console.log("Connected to the server");
+});
+
+socket.on("server message", (data) => {
+ console.log(data.message);
+});
+socket.on("taskCreated", (data) => {
+  if(userId===data.user)
+  {
+    console.log("A task has been created for you!")
+  }
+  //console.log(data.user);
+ });
+socket.emit('task', { token: '123' });
+
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
